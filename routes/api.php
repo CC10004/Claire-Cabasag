73191b2a00f1b255e6c033ffeb7816435bbd9140
Claire Cabasag <cabasagclaire009@gmail.com>
2026-02-08 22:35:03 +0800

    Initial commit of Laravel project with CRUD setup

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\UserController;
 
-Route::get('/GREET', function () {
-    return response()->json(
-        'Hello World'
-    );
+Route::prefix('users')->group(function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']); 
 });
